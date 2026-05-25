d9f0f5c6bd941a6f14491683f40067e603222ca9
shadcy <shreyashwanjari740@gmail.com>
2026-05-25 22:43:18 +0530

    fix: speed up test game and improve CLI commands

--- games/test_game.c
+++ games/test_game.c
@@ -2,9 +2,11 @@
 #include "framebuffer.h"
 #include "keyboard.h"
 #include "console.h"
+#include "font8x16.h"
+#include "gfx_console.h"
 
 static int x = 100, y = 100;
-static int dx = 2, dy = 2;
+static int dx = 5, dy = 5;
 
 static void test_init(void) {
     x = 100; y = 100;
@@ -25,9 +27,28 @@ static void test_update(int dt_ms) {
     if (kb_is_pressed('d')) x += 5;
 }
 
+static void draw_text(int x_pos, int y_pos, const char *str, uint16_t color) {
+    while (*str) {
+        unsigned char c = *str++;
+        const unsigned char *g = font8x16_data[c];
+        for (int r = 0; r < FONT8X16_HEIGHT; r++) {
+            unsigned char bits = g[r];
+            for (int b = 0; b < FONT8X16_WIDTH; b++) {
+                if (bits & (0x80u >> b)) {
+                    fb_putpixel(x_pos + b, y_pos + r, color);
+                }
+            }
+        }
+        x_pos += FONT8X16_WIDTH;
+    }
+}
+
 static void test_draw(void) {
     fb_clear(COLOR_BLACK);
     fb_fillrect(x, y, 20, 20, COLOR_RED);
+    draw_text(10, 10, "2D Engine Test", COLOR_GREEN);
+    draw_text(10, 30, "Controls: W A S D to move", COLOR_WHITE);
+    draw_text(10, 50, "Press Q to exit back to shell", COLOR_YELLOW);
 }
 
 void cmd_test_game(int argc, char **argv) {
@@ -42,6 +63,9 @@ void cmd_test_game(int argc, char **argv) {
         .draw = test_draw
     };
     engine2d_run(&app);
+    
+    /* Re-initialize console to clear screen and restore the shell layout */
+    gfx_console_init();
     
     kputs("Exited 2D Engine Test.\n");
 }
