a6c9a984d8650bf79587db4761b619fd1f6ab33c
shadcy <shreyashwanjari740@gmail.com>
2026-06-06 09:36:12 +0530

    perf(image_viewer): Cache BMP images in memory to prevent severe disk IO lag and rename Slime

--- include/bmp.h
+++ include/bmp.h
@@ -1,6 +1,9 @@
 #ifndef BMP_H
 #define BMP_H
 
+#include <stdint.h>
+
 void bmp_load_and_draw(const char *filename, int x_offset, int y_offset);
+uint16_t *bmp_load(const char *filename, int *out_w, int *out_h);
 
 #endif
--- kernel/bmp.c
+++ kernel/bmp.c
@@ -88,3 +88,52 @@ void bmp_load_and_draw(const char *filename, int x_offset, int y_offset) {
     kfree(row_buf);
     fat_close(file);
 }
+
+uint16_t *bmp_load(const char *filename, int *out_w, int *out_h) {
+    fat_file_t *file = fat_open(filename);
+    if (!file) return NULL;
+    
+    bmp_file_header_t fh;
+    if (fat_read(file, &fh, sizeof(fh)) != sizeof(fh) || fh.type != 0x4D42) {
+        fat_close(file);
+        return NULL;
+    }
+    
+    bmp_info_header_t ih;
+    if (fat_read(file, &ih, sizeof(ih)) != sizeof(ih) || ih.bpp != 16) {
+        fat_close(file);
+        return NULL;
+    }
+    
+    fat_seek(file, fh.offset);
+    
+    int w = ih.width;
+    int h = ih.height;
+    if (out_w) *out_w = w;
+    if (out_h) *out_h = h;
+    
+    uint16_t *img_buf = (uint16_t *)kmalloc(w * h * 2);
+    if (!img_buf) {
+        fat_close(file);
+        return NULL;
+    }
+    
+    int row_size = ((w * 2) + 3) & ~3;
+    uint8_t *row_buf = (uint8_t *)kmalloc(row_size);
+    if (!row_buf) {
+        kfree(img_buf);
+        fat_close(file);
+        return NULL;
+    }
+    
+    for (int y = h - 1; y >= 0; y--) {
+        if (fat_read(file, row_buf, row_size) != (uint32_t)row_size) break;
+        for (int x = 0; x < w; x++) {
+            img_buf[y * w + x] = *(uint16_t *)(&row_buf[x * 2]);
+        }
+    }
+    
+    kfree(row_buf);
+    fat_close(file);
+    return img_buf;
+}
