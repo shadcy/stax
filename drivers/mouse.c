f8749ac6cef9faef297df7b32d6d83403aebd016
shadcy <shreyashwanjari740@gmail.com>
2026-08-14 01:54:41 +0530

    stable-release

--- drivers/mouse.c
+++ drivers/mouse.c
@@ -79,9 +79,9 @@ void mouse_poll(void) {
                 mouse_y -= (rel_y * 2); /* PS/2 y is up */
                 
                 if (mouse_x < 0) mouse_x = 0;
-                if (mouse_x >= FB_WIDTH) mouse_x = FB_WIDTH - 1;
+                if (mouse_x >= (int)FB_WIDTH) mouse_x = FB_WIDTH - 1;
                 if (mouse_y < 0) mouse_y = 0;
-                if (mouse_y >= FB_HEIGHT) mouse_y = FB_HEIGHT - 1;
+                if (mouse_y >= (int)FB_HEIGHT) mouse_y = FB_HEIGHT - 1;
                 
                 mouse_buttons = mouse_byte[0] & 0x07;
                 mouse_changed = 1;
--- ui/gfx_console.c
+++ ui/gfx_console.c
@@ -190,7 +190,7 @@ void gfx_console_draw_window(struct window *win, int cx, int cy, int cw, int ch)
                     unsigned char bits = g[gr];
                     for (int gb = 0; gb < 8; gb++) {
                         if (bits & (0x80 >> gb)) {
-                            if (py+gr >= 0 && py+gr < FB_HEIGHT && px+gb >= 0 && px+gb < FB_WIDTH) {
+                            if (py+gr >= 0 && py+gr < (int)FB_HEIGHT && px+gb >= 0 && px+gb < (int)FB_WIDTH) {
                                 fbuf[(py+gr)*FB_WIDTH + (px+gb)] = color;
                             }
                         }
